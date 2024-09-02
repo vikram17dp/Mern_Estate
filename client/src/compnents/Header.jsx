615e2e724892ff18969f71780b554fd2c3484a94
vikram17dp <vikramdp505@gmail.com>
2024-09-03 02:13:13 +0530

    create header compnent

--- client/src/compnents/Header.jsx
+++ client/src/compnents/Header.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between max-w-6xl items-center mx-auto p-3'>
+      <Link to='/'>
+      <h1 className="font-bold text-sm sm:text-[2vw]  flex flex-wrap">
+        <span className="text-slate-400">Estate</span>
+        <span className="text-slate-700">Ease</span>
+      </h1>
+      </Link>
+      <form className='flex'>
+        <input type="text" placeholder='search...' className='bg-slate-100 rounded-lg p-3 border-none outline-none w-[40vw] sm:w-[24vw]' />
+        
+      </form>
+      <ul className='flex gap-4 font-semibold cursor-pointer'>
+        <Link to='/'>
+        <li className='hidden sm:inline text-slate-600 hover:underline'>Home</li>
+        
+        </Link>
+        <Link to='/about'>
+        
+        <li className='hidden sm:inline text-slate-600 hover:underline'>About</li>
+        </Link>
+        <Link to='/signin'>
+        
+        <li className='text-slate-600 hover:underline'>Signin</li>
+        </Link>
+      </ul>
+      </div>
+      
+    </header>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,9 @@ import React from "react";
 
 function Home() {
   return (
-    <header>
-      <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-        <span className="text-slate-400">Estate</span>
-        <span className="text-slate-400">Estate</span>
-      </h1>
-    </header>
+    <div>
+      Home
+    </div>
   );
 }
 
