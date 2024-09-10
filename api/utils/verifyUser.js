5dab6a435df609282c9ba8afedac7c7c6da1b931
vikram17dp <vikramdp505@gmail.com>
2024-09-11 01:17:36 +0530

    complete update the user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,7 +3,6 @@ import { errorHandler } from './error.js';
 export const verifyToken = (req,res,next)=>{
     
     const token = req.cookies.access_token;
-    console.log("Token:", token);
     if(!token) return next(errorHandler(401,'Unauthorized'));
     jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
         if(err) return next(errorHandler(403,'Forbiden'));
