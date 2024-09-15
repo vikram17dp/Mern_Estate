44261c8f3a7a018b4b256da01fe2c2ee63cc8bd0
vikram17dp <vikramdp505@gmail.com>
2024-09-15 15:41:15 +0530

    create update listing api route

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -2,7 +2,6 @@ import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
-
   try {
     const listing = await Listing.create(req.body);
 
@@ -12,21 +11,40 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(400, "Lsiting is not found"));
+  }
+  if (req.user.id !== listing.userRef.toString()) {
+    return next(errorHandler(404, "You can only delete your listing"));
+  }
 
-export const deleteListing = async(req,res,next)=>{
-    const listing = await Listing.findById(req.params.id);
-    if(!listing){
-      return next(errorHandler(400,'Lsiting is not found'))
-    }
-    if(req.user.id !== listing.userRef.toString()){
-      return next(errorHandler(404,'You can only delete your listing'))
-    }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listning is deleted Succesfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
-    try {
-      await Listing.findByIdAndDelete(req.params.id);
-      res.status(200).json('Listning is deleted Succesfully')
-    } catch (error) {
-      next(error)
-    }
+export const updatedListning = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing is not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "you can only update your account"));
+  }
 
-}
+  try {
+    const UpdatedListning = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(UpdatedListning);
+  } catch (error) {
+    next(error);
+  }
+};
