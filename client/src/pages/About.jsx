7f1714658206b9a3cdc9ec56d4e80a3fd7eeadfa
vikram17dp <vikramdp505@gmail.com>
2024-09-20 16:47:09 +0530

    about section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      About
+    <div className='w-[85%]'>
+      <h1 className='text-4xl text-slate-700 ml-[17vw] mt-20  font-bold'>About EstateEase</h1>
+      <p className='text-lg font-normal ml-[17vw] mt-10 text-slate-600'>EstateEase is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.
+      </p>
+      <p className='text-lg font-normal ml-[17vw] mt-5 text-slate-600'>Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.</p>
+      <p className='text-lg font-normal ml-[17vw] mt-4 text-slate-600'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
     </div>
   )
 }
