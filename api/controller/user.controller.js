b6c4907e39f26e9c956b8469cfdd50629c210a43
vikram17dp <vikramdp505@gmail.com>
2024-09-23 14:25:58 +0530

    small bugs

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,5 @@
 import Listing from "../models/listing.model.js";
-import  { User } from "../models/user.model.js";
+import  User  from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcryptjs";
 
@@ -46,7 +46,7 @@ export const deleteUser = async (req, res, next) => {
     return next(errorHandler(401, "You can only delete your account"));
   }
   try {
-    const userToDelete = await user.findByIdAndDelete(req.params.id);
+    const userToDelete = await User.findByIdAndDelete(req.params.id);
     if (!userToDelete) return next(errorHandler(404, "User not found"));
     res.clearCookie("access_token");
     res.status(200).json("user deleted succesfully!");
