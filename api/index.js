c62bba7f0476ebbbdc792138cf6c498deeb97398
vikram17dp <vikramdp505@gmail.com>
2024-09-30 20:05:30 +0530

    new changes

--- api/index.js
+++ api/index.js
@@ -26,11 +26,11 @@ app.use(cors())
 
 
 
-// app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
