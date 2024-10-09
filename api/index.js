3576cda9f0091b0d32b26deafc7170b79bf3855e
vikram17dp <vikramdp505@gmail.com>
2024-10-09 21:12:18 +0530

    small changes

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, '/client','dist')));
+app.use(express.static(path.join(__dirname, 'client','dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
